c9d74d3587c911c48fcb36daa90a5cc991a250a6
= <=>
2022-06-09 10:45:04 +0200

    finalisation app mobile

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -9,11 +9,11 @@ import { SiteOutilsComponent } from "./site-outils/site-outils.component";
 
 const APP_ROUTING : Routes = [
 
-  { path: '', component: AccueilComponent },
+  { path: '', component: AProposComponent },
   { path: 'site-outils', component: SiteOutilsComponent },
   { path: 'mes-competences', component: MesCompetencesComponent },
   { path: 'competence/:id', component: CompetenceComponent },
-  { path: 'a-propos', component: AProposComponent },
+  // { path: 'a-propos', component: AProposComponent },
 
 ];
 
